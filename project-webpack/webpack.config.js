bb96292f2deec937b6d42abc6d634ea9e3028958
iuukai <790331286@163.com>
2026-05-29 13:46:12 +0800

    build(vercel): 调整部署配置和webpack公共路径
    1.  修改webpack配置的publicPath为根路径
    2.  更新vercel.json配置：为静态构建添加distDir配置，优化路由匹配规则

--- project-webpack/webpack.config.js
+++ project-webpack/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/webpack/',
+		publicPath: '/',
 		filename: 'main.js'
 	},
 	plugins: [
